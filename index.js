93ef71d1b5e449d2c9696e4edc49d7713a455e1b
Anton Groznykh <antongroznykh@gmail.com>
2019-11-13 00:32:14 +0200

    Automatic layout correction and a couple of bug fixes

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const Telegraph = require('telegraf');
+const enDict = require('wordlist-english')['english'];
+const ruDict = require('wordlist-russian')['russian'];
 
 // Languages
 
 class Lang {
-    constructor(keys) {
+    constructor(keys, dict) {
         this.direct = new Map();
         keys.split('').forEach((element, index) => this.direct.set(element, index));
+
         this.reverse = new Map();
         for (let [key, value] of this.direct.entries()) {
             this.reverse.set(value, key);
-        };
+        }
+
+        this.dict = dict;
     }
 
     getIndex(char) {
@@ -31,11 +36,15 @@ class Lang {
     }
 }
 
-const enKeys = '`~1!2@3#4$5%6^7&8*9(0)-_=+qQwWeErRtTyYuUiIoOpP[{]}aAsSdDfFgGhHjJkKlL;:\'\"\\|zZxXcCvVbBnNmM,<.>/?';
-const ruKeys = 'ёЁ1!2"3№4;5%6:7?8*9(0)-_=+йЙцЦуУкКеЕнНгГшШщЩзЗхХъЪфФыЫвВаАпПрРоОлЛдДжЖэЭ\\/яЯчЧсСмМиИтТьЬбБюЮ.,';
+const enKeys = '`~1!2@3#4$5%6^7&8*9(0)-_=+qQwWeErRtTyYuUiIoOpP[{]}aAsSdDfFgGhHjJkKlL;:\'\"\\|zZxXcCvVbBnNmM,<.>/?'
+    , ruKeys = 'ёЁ1!2"3№4;5%6:7?8*9(0)-_=+йЙцЦуУкКеЕнНгГшШщЩзЗхХъЪфФыЫвВаАпПрРоОлЛдДжЖэЭ\\/яЯчЧсСмМиИтТьЬбБюЮ.,';
 
-const RU = new Lang(ruKeys);
-const EN = new Lang(enKeys);
+const RU = new Lang(ruKeys, ruDict);
+const EN = new Lang(enKeys, enDict);
+
+// Useful functions
+
+const notWordRegEx = new RegExp('(?![-А-я ])');
 
 function layoutSwitch(input) {
     return input
@@ -44,6 +53,15 @@ function layoutSwitch(input) {
         .map((element) => RU.getChar(element))
         .join('');
 }
+
+function words(text) {
+    return text
+        .toLocaleLowerCase(['en', 'ru'])
+        .replace(notWordRegEx, '')
+        .split(' ');
+}
+
+// Telegram bot
 
 const token = process.env.BOT_TOKEN;
 const bot = new Telegraph(token);
@@ -67,30 +85,68 @@ bot.on('inline_query', (ctx) => {
     }
 });
 
-let msgTracker = 0;
-let msgCurrent = 0;
-let msg = "";
-const allRegEx = new RegExp('.*');
+
+let msgTracker = {};
 
 bot.command('/switch', (ctx) => {
-    msgTracker = ctx.message.message_id;
-    ctx.reply('Send the text for switching or reply to anything you would like to switch');
+    msgTracker[ctx.chat.id] = ctx.message.message_id;
+    ctx.reply('Send a message for correction or reply to anything you would like to correct');
+});
+
+bot.command('/start', (ctx) => {
+    ctx.reply('Hi! I will automatically correct the keyboard layout of all the messages here. You can also use' +
+        '/switch to ask me to correct anything without analyzing the text');
 });
 
 bot.help((ctx) => {
-    ctx.reply('I can only do one thing. I switch the keyboard layouts of messages. For that, send /switch');
+    ctx.reply('I can only do one thing. I correct messages by switching their keyboard, automatically or with a' +
+        '/switch');
 });
 
+const ruRegEx = new RegExp('[А-я]');
+
 bot.on('message', (ctx) => {
-    msgCurrent = ctx.message.message_id;
-    if (msgCurrent === msgTracker + 2) {
-        if (typeof ctx.message.reply_to_message !== "undefined") {
-            msg = ctx.message.reply_to_message.text;
+    if (typeof ctx.message.text !== 'undefined') {
+        const chatId = ctx.chat.id;
+        const msgId = ctx.message.message_id;
+        let msg = '';
+        if (msgId === msgTracker[chatId] + 2) {
+            if (typeof ctx.message.reply_to_message !== 'undefined') {
+                msg = ctx.message.reply_to_message.text;
+            } else {
+                msg = ctx.message.text;
+            }
+            const mapped = layoutSwitch(msg);
+            ctx.telegram.sendMessage(chatId, mapped, {reply_to_message_id: msgId});
         }
         else {
             msg = ctx.message.text;
+            const onlyEn = !ruRegEx.test(msg);
+            if (onlyEn) {
+                let isValid = false;
+                const msgWords = words(msg);
+                for (const element of msgWords) {
+                    if (EN.dict.indexOf(element) !== -1 && element.length >= 3) {
+                        isValid = true;
+                        break;
+                    }
+                }
+                if (!isValid) {
+                    const mapped = layoutSwitch(msg);
+                    const mapWords = words(mapped);
+                    let match = false;
+                    for (const element of mapWords) {
+                        if (RU.dict.indexOf(element) !== -1 && element.length >= 3) {
+                            match = true;
+                            break;
+                        }
+                    }
+                    if (match) {
+                        ctx.telegram.sendMessage(chatId, mapped, {reply_to_message_id: msgId});
+                    }
+                }
+            }
         }
-        ctx.reply(layoutSwitch(msg));
     }
 });
 
